93e65f48f6acf512537c7861a983e8f8c6cff42a
JingyiNiu <niu.jy1203@gmail.com>
2023-04-03 13:39:14 +1200

    update view_count when client view article

--- controllers/article.controller.js
+++ controllers/article.controller.js
@@ -1,5 +1,4 @@
 const connection = require('../config/databse');
-const Joi = require('joi');
 
 const article_table = 'article';
 
@@ -8,17 +7,21 @@ const articleController = {
         const sql = `SELECT * FROM ${article_table}
                     WHERE is_public = 1
                     ORDER BY sort_order ASC, id DESC`;
-        const [rows, fields] = await connection.query(sql);
-        res.json({ data: rows });
+        const [results] = await connection.query(sql);
+        res.json({ data: results });
     },
 
     getOneArticle: async (req, res) => {
         const { id } = req.params;
-        const sql = `SELECT * FROM ${article_table} 
-                    WHERE (id = ? AND is_public = 1)
-                    OR (slug = ? AND is_public = 1) `;
-        const [rows, fields] = await connection.query(sql, [id, id]);
-        res.json({ data: rows });
+        const updateSql = `UPDATE ${article_table} 
+                            SET view_count = view_count + 1 
+                            WHERE id = ? OR slug = ?`;
+        const selectSql = `SELECT * FROM ${article_table} 
+                            WHERE (id = ? AND is_public = 1)
+                            OR (slug = ? AND is_public = 1)`;
+        const [updateResult] = await connection.query(updateSql, [id, id]);
+        const [selectResult] = await connection.query(selectSql, [id, id]);
+        res.json({ data: selectResult });
     },
 };
 
